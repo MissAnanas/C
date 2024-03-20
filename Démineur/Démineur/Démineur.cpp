4ec300ae75dc9e2c11edca73a13c907bd638fa84
Giron Willem <147034330+WSkaro@users.noreply.github.com>
2024-03-20 13:15:31 +0100

    gg

--- Démineur/Démineur/Démineur.cpp
+++ Démineur/Démineur/Démineur.cpp
@@ -16,6 +16,21 @@ typedef struct {
 	Tile** tile; // Tableau 2D de cases
 } Grid;
 
+/*int AskInt()
+{
+	while (1)
+	{
+		int i; 
+		int error = scanf_s("%d", &i);
+		while (getchar() != '\n');
+		if (error == 1)
+		{
+			return i;
+		}
+		printf("Veuillez rentrer un entier ! \n");
+	}
+}
+*/
 
 void InitTile(Tile* tile) {
 	tile->is_mine = 0;
@@ -43,23 +58,51 @@ void InitGrid(Grid* Grid, int row, int col) {
 	}
 }
 
+Tile* GetTile(Grid* grid, int row, int col) {
+	if (row < 0 || col < 0 || row >= grid->row || col >= grid->col);
+		return NULL;
 
-void PlaceMines(Grid* grid, int num_mines) {
+	return &grid->tile[row][col];
+}
+
+
+void PlaceMines(Grid* grid, int num_mines, int row, int col) {
 	srand(time(NULL));
 
-	int row;
-	int col;
 	int mines_placed = 0;
+	int total_size = grid->row * grid->col;
 
-	while (mines_placed < num_mines) {
-		row = rand() % grid->row;
-		col = rand() % grid->col;
+	int* available_tiles = (int*)malloc(total_size * sizeof(int));
+	if (available_tiles == NULL) exit(1);
 
-		if (grid->tile[row][col].is_mine == 0) {
-			grid->tile[row][col].is_mine = 1;
-			mines_placed++;
+	for (int i = 0; i < total_size; i++) {
+		*(available_tiles + i) = i;
+	}
+
+	for (int m = 1; m < num_mines + 1; m++) {
+		int selected_index = rand() % total_size;
+		int selected_val = available_tiles[selected_index];
+
+		available_tiles[selected_index] = available_tiles[total_size - 1];
+
+		int x = selected_val / grid->col;
+		int y = selected_val % grid->col;
+
+		grid->tile[x][y].is_mine = 1;
+
+		for (int i = -1; i <= 1; ++i) {
+			for (int j = -1; j <= 1; ++j) {
+				Tile* tile = GetTile(grid, x + i, y + j);
+				if (tile == NULL)
+					continue;
+
+				tile->close_mine++;
+			}
 		}
-	} 
+
+		total_size--;
+	}
+	
 }
 
 
@@ -134,24 +177,59 @@ void AskCoord(Grid* grid, int* rowx, int* coly) {
 	RevealTile(grid, *rowx, *coly);
 }
 
-
 int main() {
 	Grid grid;
 
 	int rows = 16;
 	int cols = 40;
 	int num_mines = 99;
+	char rejouer = 'y';
+
+
+	/*printf(" --------------------------------------------------------------------------------------------\n");
+	printf("|    DDDDDD     EEEEEEEE  M       M  IIIIIIII  N       N  EEEEEEEE  U       U  RRRRRRR       |\n");
+	printf("|    D     D    E         MM     MM      I     NN      N  E         U       U  R      R      |\n");
+	printf("|    D      D   E         M M   M M      I     N N     N  E         U       U  R      R      |\n");
+	printf("|    D      D   EEEEEE    M  M M  M      I     N  N    N  EEEEEE    U       U  RRRRRRR       |\n");
+	printf("|    D      D   E         M   M   M      I     N   N   N  E         U       U  R  R          |\n");
+	printf("|    D     D    E         M       M      I     N    N  N  E         U       U  R    R        |\n");
+	printf("|    DDDDDD     EEEEEEEE  M       M  IIIIIIII  N     N N  EEEEEEEE  UUUUUUUUU  R      R      |\n");
+	printf(" -------------------------------------------------------------------------------------------\n\n\n");
+
+
+	printf("Voici les regles du jeu : \n\n");
+	printf("- Le demineur est un jeu de logique ou le joueur doit decouvrir les cases d'un champ de mines sans les faire exploser.\n");
+	printf("- Le jeu se deroule sur un plateau compose de cases, certaines contenant des mines.\n");
+	printf("- Le joueur peut cliquer sur une case pour la decouvrir.\n");
+	printf("- Si la case decouverte contient une mine, le jeu est termine et le joueur a perdu.\n");
+	printf("- Sinon, la case revele un chiffre qui indique le nombre de mines adjacentes.\n");
+	printf("- Le joueur doit utiliser ces chiffres pour determiner l'emplacement des mines en evitant de les cliquer.\n");
+	printf("- Le jeu est gagne lorsque toutes les cases sans mines ont ete decouvertes.\n\n");
+	*/
+
 
 	InitGrid(&grid, rows, cols);
-	PlaceMines(&grid, num_mines);
+	PlaceMines(&grid, num_mines, rows, cols);
 	PrintGrid(&grid);
+
 
 	while (1) {
 		int rowx;
 		int coly;
 		AskCoord(&grid, &rowx, &coly);
 
+
+
 		PrintGrid(&grid);
+
+		/*printf("Voulez-vous rejouer ? y/n \n");
+		scanf_s("%c", &rejouer, 1);
+		while (getchar() != '\n');
+
+		if (rejouer == 'n')
+		{
+			break;
+		}*/
 	}
 	
 	return 0;
